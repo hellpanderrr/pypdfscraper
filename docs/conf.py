77f0a1be75e90392c887c5d06f60c08e9c5ce994
hellpanderrr <hellpanderrr@gmail.com>
2022-06-01 21:25:22 +0300

    testing docs

--- docs/conf.py
+++ docs/conf.py
@@ -30,8 +30,7 @@ autosummary_generate = True
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc','sphinx.ext.autosummary','sphinx.ext.coverage', 'sphinx.ext.napoleon','sphinx.ext.viewcode'
-]
+extensions = ['sphinx.ext.autodoc','sphinx.ext.autosummary','sphinx.ext.coverage', 'sphinx.ext.napoleon','sphinx.ext.viewcode','sphinx_rtd_theme']
 extensions.append('autoapi.extension')
 autoapi_type = 'python'
 autoapi_dirs = ['../pdfscraper']
@@ -51,7 +50,7 @@ exclude_patterns = ['_build', '_templates','Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
